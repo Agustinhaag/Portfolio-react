e30f8a6c2b1a43c6f8d1d0e6938081cee3d7b683
Agustin Haag <122823252+Agustinhaag@users.noreply.github.com>
2024-02-13 18:11:13 -0300

    corrección de dark-mode

--- src/pages/Github.jsx
+++ src/pages/Github.jsx
@@ -2,8 +2,10 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { useDarkMode } from './context/ModeContext';
 
 const Github = () => {
+  const {mode} = useDarkMode()
     const [user, setUser] = useState([]);
     useEffect(()=>{
         const userGithub = async () => {
@@ -13,17 +15,17 @@ const Github = () => {
           };
           userGithub();
     },[]) 
-    console.log(user)
+    
   return (
     <div>
       <Navbar/>
       <div className='cont-github flex flex-col items-center w-4/5 my-0 mx-auto'>
       <div className='cont-gral flex'>
-         <div className='cont-img-git p-1 w-2/5 h-2/5'>
+         <div className='cont-img-git p-1 w-2/4 h-96'>
         <img src={user.avatar_url} alt={user.name} className='h-full w-full'/>
       </div>
 
-      <div className='cont-info'>
+      <div className='cont-info w-full'>
         <h4>{user.name}</h4>
         <p><span>Biografía:</span> {user.bio}</p>
         <p><span>Cantidad de repos:</span> {user.public_repos}</p>
@@ -36,8 +38,8 @@ const Github = () => {
      
         
         <div className='cont-btn-git flex justify-between mt-12'>
-          <Link className='git w-1/4 py-2.5 px-5' to={user.html_url}>Github</Link>
-          <Link className='linkedin py-2.5 px-5' to={user.blog}>Linkedin</Link>
+          <Link className={mode ? 'git py-2.5 px-5 text-white' : 'git  py-2.5 px-5 text-customBlack'} to={user.html_url}>Github</Link>
+          <Link className={mode ? 'linkedin py-2.5 px-5 text-white': 'linkedin py-2.5 px-5 text-customBlack'} to={user.blog}>Linkedin</Link>
         </div>
         
     </div>
